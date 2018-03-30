6ae5fa4619a86199f3af51319290d7c6cbf0b0cf
Freaper87 <freaper@gmx.de>
2018-03-30 11:58:44 +0200

    First version

---
+++ web/js/index.js
@@ -0,0 +1,42 @@
+var c = document.getElementById('canv');
+var $ = c.getContext('2d');
+
+var w = c.width = window.innerWidth;
+var h = c.height = window.innerHeight;
+var my_gradient=$.createLinearGradient(0,0,0,940);
+my_gradient.addColorStop(0,"#7953d2");
+my_gradient.addColorStop(1,"#000070");
+
+var draw = function(a, b, t) {
+  $.lineWidth = 0.5;
+  $.fillStyle = my_gradient; //'hsla(0,0%,70%,1)';
+  $.fillRect(0, 0, w, h);
+  for (var i = -60; i < 30; i += 1) {
+    $.strokeStyle = 'hsla(85, 25%, 100%, 0.1)';
+    $.beginPath();
+    $.moveTo(0, h);
+    for (var j = -10; j < w; j += 7) {
+      $.lineTo(10 * Math.cos(i / 10) +
+        j + 0.008 * j * j,
+        Math.floor(h / 2 + j / 2 *
+          Math.sin(j / 50 - t / 80 - i / 118) +
+          (i * 0.6) * Math.cos(j / 25 - (i + t) / 65)));
+    };
+    $.stroke();
+  }
+}
+var t = 0;
+
+window.addEventListener('resize', function() {
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+  $.fillStyle = 'hsla(277, 95%, 55%, 1)';
+}, false);
+
+var run = function() {
+  window.requestAnimationFrame(run);
+  t += 1.2;
+  draw(33, 52 * Math.sin(t / 400), t);
+};
+
+run();
