f5e192ec80f32bbc38f3283f4eb4960e4ad9a1e4
Freaper87 <freaper@gmx.de>
2018-04-02 11:33:23 +0200

    layout 2

--- web/js/parallax.js
+++ web/js/parallax.js
@@ -1,22 +1,15 @@
-(function() {
-  // Tutorial: https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27
-  window.addEventListener('scroll', function(event) {
-    var depth, i, layer, layers, len, movement, topDistance, translate3d;
-    topDistance = this.pageYOffset;
-    layers = document.querySelectorAll("[data-type='parallax']");
-    for (i = 0, len = layers.length; i < len; i++) {if (window.CP.shouldStopExecution(1)){break;}
-      layer = layers[i];
-      depth = layer.getAttribute('data-depth');
-      movement = -(topDistance * depth);
-      translate3d = 'translate3d(0, ' + movement + 'px, 0)';
-      layer.style['-webkit-transform'] = translate3d;
-      layer.style['-moz-transform'] = translate3d;
-      layer.style['-ms-transform'] = translate3d;
-      layer.style['-o-transform'] = translate3d;
-      layer.style.transform = translate3d;
-    }
-window.CP.exitedLoop(1);
-
-  });
-
-}).call(this);
+window.addEventListener('scroll', function(event) {
+  const topDistance = this.pageYOffset; 
+  const layers = document.querySelectorAll("[data-type='parallax']");
+  
+  for (let layer of Array.from(layers)) {
+    const depth = layer.getAttribute('data-depth');
+    const movement = -(topDistance * depth);
+    const translate3d = `translate3d(0, ${movement}px, 0)`;
+    layer.style['-webkit-transform'] = translate3d;
+    layer.style['-moz-transform'] = translate3d;
+    layer.style['-ms-transform'] = translate3d;
+    layer.style['-o-transform'] = translate3d;
+    layer.style.transform = translate3d;
+  }
+});
